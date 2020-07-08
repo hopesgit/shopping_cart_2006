04317fbd56a74f23c885df1a131bc9d504c46ce5
Hope Gochnour <hope.gochnour@gmail.com>
2020-07-08 10:57:54 -0600

    Add Iteration 3 content

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -19,4 +19,26 @@ class ShoppingCart
       @name => @capacity
     }
   end
+
+  def total_number_of_products
+    total_product = 0
+    products.map do |product|
+      total_product += product.quantity.to_i
+    end
+    return total_product
+  end
+
+  def is_full?
+    products.count == capacity
+  end
+
+  def products_by_category(cat)
+    category_names = []
+    products.find_all do |product|
+      if product.category == cat
+        category_names << product.name
+      end
+    end
+    return category_names
+  end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -33,4 +33,37 @@ class ShoppingCartTest < Minitest::Test
     assert_equal ["King Soopers"], cart.details.keys
   end
 
+  def test_cart_it_can_total
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 13, cart.total_number_of_products
+  end
+
+  def test_cart_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal false, cart.is_full?
+  end
+
+  def test_product_categories_in_cart
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal ["toilet paper", "tissue paper"], cart.products_by_category(:paper)
+  end
 end
